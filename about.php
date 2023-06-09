aae01f313f11270f7ac58494860cd337fc6822b5
AAVanierWebDesign2k23 <134743035+AAVanierWebDesign2k23@users.noreply.github.com>
2023-06-09 03:22:27 -0400

    Commentary Notes:
    This is the website for the project.

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About - Class Project - Web Development Env. 1</title>
+
+	<link rel="stylesheet" type="text/css" href="css/about.css">
+
+</head>
+<body>
+
+    <header class="header">         <!--HEADER-->
+        <div class="container">
+            <div class="header-flex">
+                <a href="index.php" class="button1" target="blank">Aaron Asuncion</a>
+            </div>
+        </div>
+
+        <a href="about.php" class="button2" target="blank">About Me</a>
+    </header>
+	
+    <main>
+        <section class="front-page">       <!--FRONT-PAGE-->
+		    <div class="textbox">
+                <h2>About Me</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellendus ad aperiam. Natus aperiam quia officiis a ut culpa ex fugiat ea voluptas dignissimos provident non impedit, doloremque eaque porro?<br><br> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis itaque officia quos enim quaerat quae quas dicta praesentium sapiente, ex earum. Quos magni ullam quasi inventore sapiente! Dolores, accusantium repellat?</p>
+            </div>
+	    </section>
+    </main>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mainpage - Class Project - Web Development Env. 1</title>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+
+    <header class="header">
+        <div class="container">
+            <div class="header-flex">
+                <a href="index.php" class="button1" target="blank">Aaron Asuncion</a>
+            </div>
+        </div>
+
+        <a href="about.php" class="button2" target="blank">About Me</a>
+    </header>
+		
+	<section class="home-page">
+		<div class="textbox">
+            <h1>Welcome</h1>
+            <h2>Aaron Asuncion</h2>
+            <p>website</p>
+        </div>
+	</section>
+
+</body>
+</html>
